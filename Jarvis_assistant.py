41b14af1995cd731ff21f45f5668794f088b2fbf
Unleashed1 <federico.mastrogiacomo@yahoo.com>
2019-05-06 21:34:05 +0200

    da sistemare matching di stringhe

--- Jarvis_assistant.py
+++ Jarvis_assistant.py
@@ -8,6 +8,10 @@ import datetime
 import wolframalpha
 import os
 import sys
+#inserire qui sotto i vostri dati da usare durante l'utilizzo dell'assistente vocale
+nome ='vostro nome'
+mail = 'vostra mail'
+rubrica = {}
 #le parole non sono scritte correttamente per addolcire il suono
 
 engine = pyttsx3.init('sapi5')
@@ -15,7 +19,7 @@ engine = pyttsx3.init('sapi5')
 client = wolframalpha.Client('Your_App_ID')
 
 voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[len(voices)-1].id)
+engine.setProperty('voice', voices[0].id)
 
 def speak(audio):
     print('Jarvis: ' + audio)
@@ -35,20 +39,21 @@ def greetMe():
 
 greetMe()
 
-speak('Sono il suo assistente, Jarvis')
-speak('Come posso aiutarla?')
+speak('Sono la sua assistente, Jarvis')
 
 
 def myCommand():
-   
-    r = sr.Recognizer()                                                                                   
-    with sr.Microphone() as source:   
-        speak('mi dica sono in ascolto...')
-        r.pause_threshold =  3
-        audio = r.listen(source)
+    recognizer_instance = sr.Recognizer()
+    
+    with sr.Microphone() as source:
+        recognizer_instance.adjust_for_ambient_noise(source)
+        speak('Come posso aiutarla?')
+        recognizer_instance.pause_threshold = 1
+        audio = recognizer_instance.listen(source)
+        print("Listening...")
     try:
-        query = r.recognize_google(audio, language='it-IT')
-        print('User: ' + query + '\n')
+        query = recognizer_instance.recognize_google(audio, language='it-IT')
+        print(nome + ': ' + query + '\n')
         
     except sr.UnknownValueError:
         speak('Mi scusi non ho capito, potrebbe ripetere?')
@@ -66,17 +71,17 @@ if __name__ == '__main__':
         
         if 'apri youtube' in query:
             speak('okay')
-            webbrowser.open('www.youtube.com')    
+            webbrowser.open('www.youtube.it')    
 
         elif 'Jarvis apri google' in query:
             speak('okay')
-            webbrowser.open('www.google.co.in')
+            webbrowser.open('www.google.it')
 
         elif 'Jarvis apri g mail' in query:
             speak('okay')
-            webbrowser.open('www.gmail.com')
+            webbrowser.open('www.gmail.it')
 
-        elif "Jarvis come va?" in query or 'Jarvis come stai?' in query:
+        elif 'Jarvis come va' in query or "Jarvis come va?" in query or 'Jarvis come stai?' in query:
             stMsgs = ['Hail idra!, ops questo non avrei dovuto dirlo!', 'Sto bene grazie signore', 'Bene!', 'Tutto ok e pronto ad eseguire i suoi ordini signore']
             speak(random.choice(stMsgs))
 
@@ -95,7 +100,7 @@ if __name__ == '__main__':
                     server.login("mail", 'pass')
                     server.sendmail('Your_Username', "Recipient_Username", content)
                     server.close()
-                    speak("La sua mail Ã¨ stata inviata con successo!")
+                    speak("La sua mail è¨ stata inviata con successo!")
 
                 except:
                     speak('Mi dispiace signore i chitauri hanno attaccato i server google, non sono in grado di inviare la mail!')
@@ -106,8 +111,8 @@ if __name__ == '__main__':
             speak('A presto signore, buona giornata.')
             sys.exit()
            
-        elif 'Ciao Jarvis' in query:
-            speak('Buongiorno signore!')
+        elif ' ciao jarvis' in query:
+            speak('Salve signore!')
 
         elif 'Grazie jarvis' in query or 'Grazie' in query or '':
             speak('Arrivederci signore, le auguro una buona giornata.')
@@ -128,11 +133,11 @@ if __name__ == '__main__':
                     
                 except:
                     results = wikipedia.summary(query, sentences=2)
-                    speak('Got it.')
+                    speak('Ci siamo.')
                     speak('WIKIPEDIA dice  - ')
                     speak(results)
         
             except:
-                webbrowser.open('www.google.com')
+                webbrowser.open('www.google.it')
         
         speak('Attendo un suo comando...')
